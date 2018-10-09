6dde53403f1510d29ae8a543bb01484049cae21f
Joshua Torres <torresj1997@outlook.com>
2018-10-09 14:35:56 -0500

    Initial commit

---
+++ noob_bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = " ")
+
+@client.event
+async def on_ready():
+    print("Bot is ready!")
+
+@client.event
+async def on_message(message):
+    if message.content.upper().startswith('!PING'):
+        userID = message.author.id
+        # Pong the user back!
+        await client.send_message(message.channel, "<@%s> Pong!" % (userID))
+    if message.content.upper().startswith('!SAY'):
+        args = message.content.split(" ")
+        # Output a formatted sentence
+        await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
+
+client.run("NDk5MjkxMjY1MTgwMTA2NzUy.Dp6JQQ.AYDXUFkhX0Gej0RwxD-YqF2p1JQ")
