692547e3f13037c7569140751011477bb0e496c6
Joshua Torres <torresj1997@outlook.com>
2018-10-12 22:08:00 -0500

    Get user avatar

--- cogs/basic.py
+++ cogs/basic.py
@@ -11,7 +11,7 @@ class BasicCog:
     # Replies back to the command context with the text "Pong!"
     # .ping
     @commands.cooldown(1, 1, commands.BucketType.user)
-    @commands.command()
+    @commands.command(name='ping')
     async def ping(self, ctx):
         await ctx.send("Pong!")
 
@@ -20,13 +20,24 @@ class BasicCog:
     @commands.command(name='me')
     @commands.is_owner()
     async def only_me(self, ctx):
-        await ctx.send(f'Hello {ctx.author.mention}. This command can only be used by you!!')
+        await ctx.send(f'Hello {ctx.author.mention}. This command can only be used by you!')
 
     # Is the bot cool?
     # .coolbot
     @commands.command(name='coolbot')
     async def cool_bot(self, ctx):
         await ctx.send('This bot is cool. :)')
+
+    # Gets the avatar of a selected user
+    # .getAvatar .getAvatar @user
+    @commands.cooldown(1, 1, commands.BucketType.user)
+    @commands.command(name='getAvatar')
+    async def getAvatar(self, ctx, searchUser):
+        result = ""
+        for member in ctx.guild.members:
+            if member.mention == searchUser:
+                result = member.avatar_url
+        await ctx.send(f'{searchUser}\'s avatar is: {result}')
 
 # End of class BasicCog
 
