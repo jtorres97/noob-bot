39d306cdd313b57077352fa5cca159613823da7e
Joshua Torres <torresj1997@outlook.com>
2018-11-13 21:29:47 -0600

    List all members in a server

--- cogs/user.py
+++ cogs/user.py
@@ -28,6 +28,19 @@ class UserCog:
         embed.add_field(name="Account Created at: ",value=user.created_at,inline=True)
         await ctx.send(embed=embed)
 
+    # Lists every member in the server, along with their discord id
+    # .listMembers
+    @commands.command(name="listMembers")
+    async def listMembers(self, ctx):
+        member_string = "\n"
+        for member in ctx.guild.members:
+            member_string += ("\t" + '{:32}'.format(member.name) + ":\t(id = " + str(member.id) + ")\n")
+        server_string = "Server Name: {}\nMembers:\n    ".format(ctx.guild.name)
+        result = "```"
+        result += ("\n" + server_string + member_string)
+        result += ("\n```")
+        await ctx.send(result)
+
 # End of class UserCog
 
 # Add this cog to the bot
